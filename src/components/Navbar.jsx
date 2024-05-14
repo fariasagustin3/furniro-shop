7681564a960a6009991dc148d8a2f26e97da73ed
fariasagustin3 <fariasagustin3@gmail.com>
2024-05-14 11:30:20 -0300

    fix(body): responsive overflow x now is hidden

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom"
 
 const Navbar = () => {
   return (
-    <div className="px-20 py-8 w-full flex items-center justify-between">
+    <div className="md:px-20 px-5 py-8 w-full flex items-center justify-between">
       {/* logo - left */}
       <div>
         <img src="/logo.png" className="w-32" />
@@ -21,7 +21,7 @@ const Navbar = () => {
       </ul>
 
       {/* buttons - right */}
-      <ul className="hidden md:flex items-center gap-4">
+      <ul className="flex items-center gap-4">
         <li className="cursor-pointer">
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
             <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z" />
